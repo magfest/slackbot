da91c3b9e8a00860df91681f4bb273637fde40a3
Daniel Evans <me@danielarevans.com>
2016-03-12 14:50:20 -0800

    better bash file

--- slackbot/plugins/update.sh
+++ slackbot/plugins/update.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
+if [[ $ == 0 ]]; then
 for d in */; do
     cd $d
     git fetch
     git pull
     cd ..
 done
+else
+    for d in "$@"; do
+      cd $d
+      git fetch
+      git pull
+      cd ..
+    done
+fi
