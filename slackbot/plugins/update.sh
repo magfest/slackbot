b54e24b3ec0a2d59c03cd4492565b0bff3c2f4f5
Daniel Evans <me@danielarevans.com>
2016-03-12 14:52:17 -0800

    bash updates

--- slackbot/plugins/update.sh
+++ slackbot/plugins/update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [[ $ == 0 ]]; then
 for d in */; do
+    echo $d
     cd $d
     git fetch
     git pull
@@ -8,6 +9,7 @@ for d in */; do
 done
 else
     for d in "$@"; do
+      echo $d
       cd $d
       git fetch
       git pull
